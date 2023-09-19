4478f33644a5f1a547b30fce1245e5b5e17b68f9
Marcos López <marcoseduardo2604@gmail.com>
2023-09-18 23:00:47 -0700

    Changed API url to aim to "production" api.

--- pages/pokemon/pokemon_script.js
+++ pages/pokemon/pokemon_script.js
@@ -11,7 +11,8 @@ const body = document.body;
 const params = getQueryParameters();
 const pokemonName = params.get('pokemonName');
 
-const BASE_API_URL = "http://127.0.0.1:8081";
+//const BASE_API_URL = "http://127.0.0.1:8081";
+const BASE_API_URL = "https://thoughtful-girdle-frog.cyclic.app";
 const full_info_url = `${BASE_API_URL}/api/pokemon/full?pokemonName=${pokemonName}`;
 const currentURL = window.location.href;
 const urlParts = currentURL.split('/');
